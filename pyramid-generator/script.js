d0b7eb8d3b35071bc3268b3f4379de857363096f
vyom-cpp <vyom.sutariya0948@gmail.com>
2024-05-11 10:16:41 +0530

    Added Pyramid Generator

--- pyramid-generator/script.js
+++ pyramid-generator/script.js
@@ -1,13 +1,19 @@
-const character = "#";
-const count = 8;
+const character = "!";
+const count = 10;
 const rows = [];
+let inverted = false;
+// Make inverted to true to see the reverse pattern
 
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(padRow(i + 1, count));
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
 }
 
 let result = ""
